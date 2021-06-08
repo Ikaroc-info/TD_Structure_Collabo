34eb8384ab42f044ba54f32848bbb5a1e63c3a88
Ikaroc-info <leobertrand641@gmail.com>
2021-06-08 16:46:18 +0200

    fun

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Date.h"
+#include "Person.h"
 
 int main()
 {
-    Date* Anniv= create_date(18,12,2000);
+    Date *Anniv = create_date(18, 12, 2000);
     print_Date(Anniv);
+    Person *Boby = create_Person("Lefast", "Roger", Anniv);
+    print_Person(Boby);
 }
